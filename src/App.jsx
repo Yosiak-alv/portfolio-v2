f9b9cb03ba175ca664b2b34f0219281cc841de11
Yosiak-alv <Josias_romero60@outlook.com>
2023-11-23 11:15:48 -0600

    Update App.jsx
    with aos problem with problem in navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,33 @@
-import React from 'react';
-import { NavBar } from './components/sections/NavBar';
+import React,{useEffect} from 'react';
+import { NavBar } from './components/NavBar';
+
+import { Route } from 'react-router';
+import { Routes } from 'react-router';
+import { Home } from './pages/Home';
+import { Projects } from './pages/Projects';
+import { About } from './pages/About';
+import { Contact } from './pages/Contact';
+import { Footer } from './components/Footer';
+import Aos from "aos";
+import 'aos/dist/aos.css'
 
 export default function App(){
+  useEffect(() =>{
+    Aos.init({
+      duration: 1000, 
+      easing: 'linear',
+    });
+  },[]);
   return (
-    <NavBar />
+    <div className='bg-[#282C33]'>
+      <NavBar />
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/projects' element={<Projects/>}/>
+        <Route path='/about-me' element={<About/>}/>
+        <Route path='/contact-me' element={<Contact/>}/>
+      </Routes>
+      <Footer/>
+    </div>
   )
 }
