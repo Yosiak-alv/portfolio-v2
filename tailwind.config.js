358a20cbdc94d4b4e09de823be8c75e73326f57c
Yosiak-alv <Josias_romero60@outlook.com>
2023-11-23 23:17:07 -0600

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,23 @@ export default {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'customBlue':{
+            DEFAULT: '#406EBE',
+          '50': '#E6F0FF',
+          '100': '#C0D9FF',
+          '200': '#8AB3FF',
+          '300': '#559CFF',
+          '400': '#2F85FF',
+          '500': '#2D7FF9',
+          '600': '#1F5FC0',
+          '700': '#164080',
+          '800': '#0D2B40',
+          '900': '#051300'
+        }
+      }
+    },
   },
   plugins: [],
 }
