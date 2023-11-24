95795908d1d070bfc58da4c15c64531e14c53178
Yosiak-alv <Josias_romero60@outlook.com>
2023-11-23 23:17:53 -0600

    Scroll on top on click new page

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,28 @@
 import React,{useEffect} from 'react';
 import { NavBar } from './components/NavBar';
 
-import { Route } from 'react-router';
+import { Route ,useLocation} from 'react-router';
 import { Routes } from 'react-router';
 import { Home } from './pages/Home';
 import { Projects } from './pages/Projects';
 import { About } from './pages/About';
 import { Contact } from './pages/Contact';
 import { Footer } from './components/Footer';
+
 import Aos from "aos";
 import 'aos/dist/aos.css'
 
 export default function App(){
-  useEffect(() =>{
+  //scroll to top on route change
+  const location = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location.pathname]);
+  /* useEffect(() =>{
     Aos.init({
-      duration: 1000, 
-      easing: 'linear',
+      duration:1000
     });
-  },[]);
+  },[]); */
   return (
     <div className='bg-[#282C33]'>
       <NavBar />
